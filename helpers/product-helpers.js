e208692037b5de1108d3ed92b5c4f3dd6615ffd4
Munavvar Sinan <munavvarsinan01@gmail.com>
2020-11-06 17:27:42 +0530

    initial commit

---
+++ helpers/product-helpers.js
@@ -0,0 +1,53 @@
+var db=require('../config/connection')
+var collection=require('../config/collections')
+
+var objectId=require('mongodb').ObjectID
+module.exports={
+    addProduct:(product,callback)=>{
+        
+        db.get().collection('product').insertOne(product).then((data)=>{
+           
+            callback(data.ops[0]._id)
+        })
+    },
+    getAllProducts:()=>{
+
+    return new Promise(async (resolve,reject )=>{
+        let products=await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
+        resolve(products)
+    })
+    },
+    deleteProduct:(proId)=>{
+        return new Promise((resolve,reject)=>{
+            db.get().collection(collection.PRODUCT_COLLECTION).removeOne({_id:objectId(proId)}).then((response)=>{
+               console.log(response);
+                resolve(response)
+                
+            })
+        }) 
+
+    },
+    getProductDetails:(proId)=>{
+        return new Promise((resolve,reject)=>{
+            db.get().collection(collection.PRODUCT_COLLECTION).findOne({_id:objectId(proId)}).then((product)=>{
+                resolve(product)
+            })
+        })
+    },
+    updateProduct:(proId,proDetails)=>{
+        return new Promise((resolve,reject)=>{
+            db.get().collection(collection.PRODUCT_COLLECTION)
+            .updateOne({_id:objectId(proId)},{
+                $set:{
+                    name:proDetails.name,
+                    Discription:proDetails.Discription,
+                    Price:proDetails.Discription,
+                    Category:proDetails.Category
+
+                }
+            }).then((response)=>{
+                resolve()
+            })
+        })
+    }
+}
